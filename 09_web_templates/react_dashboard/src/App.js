5379bc3f9fa57f1b73f3339f9cd25a42ab609975
Sameer <siddheshvikram1@gmail.com>
2026-05-13 02:17:55 +0530

    Add 09_web_templates/react_dashboard/src/App.js

---
+++ 09_web_templates/react_dashboard/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Dashboard from "./components/Dashboard";
+
+function App() {
+  return (
+    <div style={{ margin: "2rem" }}>
+      <h1>Demo Dashboard</h1>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
